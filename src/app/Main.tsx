89ec959737c2562f8d73a0caaa77b43eb8aa6d93
Segrey Kiselev <kissergey98@mail.ru>
2022-10-28 19:48:17 +0300

    fsd structure apply

---
+++ src/app/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from '../pages/homepage/Homepage';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+      <div>About</div>
+      <div>Start game</div>
+      <div>Rules</div>
+      <div>Footer</div>
+    </>
+  );
+};
+
+export default Homepage;
